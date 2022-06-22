8806aac2a5be9376b02381a50e2290c2cd906ad5
wki3 <walaa.jamaluddin@eng.ku.edu.kw>
2022-06-22 20:05:16 +0300

    code file

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+string heroName = "WALAAAAA";
+int heroHeight = 161;
+int heroAge = 21;
+string heroSuperPower = "invisibilty";
+
+
+string villainName = "OREOOO";
+int villainHeight = 25;
+int villainAge = 1;
+string villainSuperPower = " sharp claws & cutness";
+
+
+int ageDifference = heroAge - villainAge;
+
+Console.WriteLine("Hero Name is "+ heroName + " , Hero Age is " + heroAge + " , Hero Height is " + heroHeight + " , Hero's super power is " +heroSuperPower );
+Console.WriteLine("Villain name is " + villainName + " , Villain Age is " + villainAge + " , Villain Height is " + villainHeight + " , Villain's super power is " + villainSuperPower);
+
+
+heroHeight += 5;
+villainSuperPower= "None";
+Console.WriteLine(" ");
+Console.WriteLine("Hero Name is "+ heroName + " , Hero Age is " + heroAge + " , Hero Height is " + heroHeight + " , Hero's super power is " +heroSuperPower );
+Console.WriteLine("Villain name is " + villainName + " , Villain Age is " + villainAge + " , Villain Height is " + villainHeight + " , Villain's super power is " + villainSuperPower);
